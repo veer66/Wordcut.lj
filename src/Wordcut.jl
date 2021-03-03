7dabc7cd78201cd1314260c25cda9fe24ae362be
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-03-04 00:23:01 +0700

    feat(wordcut): Add Latin Transducer

--- src/Wordcut.jl
+++ src/Wordcut.jl
@@ -49,4 +49,65 @@ function lookup(tree::PrefixTree, row_no, offset, ch::Char)::Union{Nothing,NodeP
     end
 end
 
+@enum LinkKind latin=1
+
+struct Link
+    p::Int64
+    w::Int64
+    unk::Int64
+    kind::LinkKind
+end
+
+function isbetter(l::Link, r::Link)::Bool
+    if r.unk < l.unk
+        return true
+    end
+
+    if r.w < l.w
+        return true 
+    end
+
+    return false
+end
+
+struct LatinTransducer
+    s::Int64
+    e::Int64
+    flag::Bool
+end
+
+@inline function islatin(ch::Char)
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')
+end
+
+function update(t::LatinTransducer, ch::Char, i::Int64, s::String)
+    if !t.flag
+        if islatin(ch)
+            t.s = i
+            t.flag = true
+        end
+    else
+        if islatin(ch)
+            if i == length(s) || !islatin(s[i + 1])
+                t.e = i
+            end
+        else
+            t.flag = false
+        end
+    end
+end
+
+function create_link(t::LatinTransducer, path::Array{Link})::Union{Nothing, Link}
+    if t.flag
+        p_link = path[t.s]
+        return Link(t.s, p_link.w + 1, p_link.unk, latin)
+    else
+        return nothing
+    end
+end
+
+#struct Punc
+
+
+
 end # module
